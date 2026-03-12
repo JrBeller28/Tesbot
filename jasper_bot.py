61ed2d18117a98a4fdb15f2130cb9ca47f5be81b
PrasetyoAdji28 <95466718+JrBeller28@users.noreply.github.com>
2026-03-12 12:17:08 +0700

    Update jasper_bot.py

--- jasper_bot.py
+++ jasper_bot.py
@@ -380,7 +380,7 @@ BOT74_REPORT_URL = (
     "&reportUnit=/iDempiere/Inventory/Stock/MaterialTransactionSummary"
     "&standAlone=true"
 )
-BOT74_WAREHOUSE_GROUP = "SCM WH POK"
+BOT74_WAREHOUSE_GROUP = "SCM WHS POK"
 
 def fill_date_v74(driver, label, index):
     print(f"  📅  {label} → '{TODAY_STR}'")
